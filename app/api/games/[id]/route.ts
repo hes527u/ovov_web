44c7239dc23b37f60a2c90376dab693b198bda9b
ovoveun <hes527u@gmail.com>
2025-04-14 22:20:44 +0900

    Netlify 빌드 오류 수정

--- app/api/games/[id]/route.ts
+++ app/api/games/[id]/route.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { games } from '@/app/data/games';
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const game = games.find((game) => game.id === params.id);
+  const game = games.find((g) => g.id === params.id);
 
   if (!game) {
     return NextResponse.json({ error: 'Game not found' }, { status: 404 });
