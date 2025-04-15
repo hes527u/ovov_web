730f596c3251f0a107b083f53c9c17bf353dc4f0
ovoveun <hes527u@gmail.com>
2025-04-15 22:24:23 +0900

    Git LFS 적용

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  output: 'standalone',
-}
-
-module.exports = nextConfig 
+version https://git-lfs.github.com/spec/v1
+oid sha256:40b8532914a634386eb84e94f83df4209144dcf0f6710aa61b8d294213e6dead
+size 157
